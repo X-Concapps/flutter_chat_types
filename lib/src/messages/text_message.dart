cfbbf8480b74a7b5472b11062dc9dad42d5d9827
aobolentsev <aobolentsev@gmail.com>
2024-05-31 09:26:31 +0200

    Add translation to TextMessage factory from Partial

--- lib/src/messages/text_message.dart
+++ lib/src/messages/text_message.dart
@@ -63,6 +63,8 @@ abstract class TextMessage extends Message {
     bool? showStatus,
     Status? status,
     int? updatedAt,
+    TextMessageTranslationState translationState =
+        const TextMessageTranslationStateEmpty(),
   }) =>
       _TextMessage(
         author: author,
@@ -76,6 +78,7 @@ abstract class TextMessage extends Message {
         showStatus: showStatus,
         status: status,
         text: partialText.text,
+        translationState: translationState,
         type: MessageType.text,
         updatedAt: updatedAt,
       );
